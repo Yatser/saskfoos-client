01e1795941e010d90e332d986b79d68f2cccdc0e
byates-va <byates@vendasta.com>
2020-04-15 21:36:46 -0600

    Fix the build warnings

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBFm-R1VRbVH3dKc8y5oVwPQVNHQPuUlWc",
-    authDomain: "saskfoos.firebaseapp.com",
-    databaseURL: "https://saskfoos.firebaseio.com",
-    projectId: "saskfoos",
-    storageBucket: "saskfoos.appspot.com",
-    messagingSenderId: "1003337708117",
-    appId: "1:1003337708117:web:ae37909845fd90e622dadc",
-    measurementId: "G-0Y9M7ENXYW"
-  };
+  apiKey: "AIzaSyBFm-R1VRbVH3dKc8y5oVwPQVNHQPuUlWc",
+  authDomain: "saskfoos.firebaseapp.com",
+  databaseURL: "https://saskfoos.firebaseio.com",
+  projectId: "saskfoos",
+  storageBucket: "saskfoos.appspot.com",
+  messagingSenderId: "1003337708117",
+  appId: "1:1003337708117:web:ae37909845fd90e622dadc",
+  measurementId: "G-0Y9M7ENXYW",
+};
 
 firebase.initializeApp(firebaseConfig);
 
-export const db = firebase.firestore();
+const db = firebase.firestore();
+db.settings({ host: "localhost:8080", ssl: false });
+export { db };
